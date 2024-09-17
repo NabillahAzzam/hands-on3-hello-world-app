52e6631724f1bc3a0b7ccbe005ff0d39ea414c1a
Azzam <azzamluthfi4@gmail.com>
2024-09-17 11:11:29 +0700

    th comm

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const	request	=	require('supertest');
 const	express	=	require('express');
 const	app	=	express();
 app.get('/', (req, res)	=>	{
-    res.send('Hello	World!');
+    res.send('Hello World!');
 });
 describe('GET /', () =>	{
     it('should return Hello World',	async () =>	{
